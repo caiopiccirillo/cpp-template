999fa7cc5b5f1c6b76b15eb0f94f6610f59b612c
Caio Piccirillo <caiopiccirillo@gmail.com>
2023-07-08 12:17:33 +0000

    Drop CMake functionality in this branch

--- .devcontainer/install-bazel.sh
+++ .devcontainer/install-bazel.sh
@@ -2,6 +2,7 @@
 set -e
 
 BAZEL_VERSION=${1:-"none"}
+BAZEL_BUILDTOOLS_VERSION=${2:-"none"}
 
 if [ "${BAZEL_VERSION}" = "none" ]; then
     echo "No Bazel version specified, skipping Bazel reinstallation"
@@ -13,4 +14,11 @@ mkdir -p /opt/bazel
 cd /opt/bazel
 wget -O bazel.sh https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 chmod +x bazel.sh
-./bazel.sh 
+./bazel.sh
+
+if [ "${BAZEL_BUILDTOOLS_VERSION}" != "none" ]; then
+    wget -O buildifier https://github.com/bazelbuild/buildtools/releases/download/${BAZEL_BUILDTOOLS_VERSION}/buildifier-linux-amd64
+    wget -O buildozer https://github.com/bazelbuild/buildtools/releases/download/${BAZEL_BUILDTOOLS_VERSION}/buildozer-linux-amd64
+    chmod +x {buildifier,buildozer}
+    mv {buildifier,buildozer} /usr/local/bin
+fi
